b17001bb5ec47e571326138cde87acdcb36af699
adityasingh2929 <2022aditya29@gmail.com>
2026-03-23 01:21:35 +0530

    initial commit, todo frontend and api setup done in clientside code.

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Productivity Manager: ")
+st.write("The purpose of this application is to help you improve in different aspects of your life by tracking,analysis and providing a good environment to keep you consistent.")
+
+st.write('')
+
+st.subheader("A guide to use this app: ")
---
+++ apis/todo_api.py
@@ -0,0 +1,32 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:8000/"
+
+def get_todos():
+    response = requests.get(f"{BASE_URL}/todos")
+    return response.json()
+
+def create_todo(todo_detail):
+    response = requests.post(
+        f"{BASE_URL}/todos",
+        json={
+            'todo_detail' : todo_detail
+        }
+    )
+    return response.json()
+
+def update_todo(todo_id,todo_detail):
+    response = requests.put(
+        f"{BASE_URL}/todos",
+        params= {'todo_id': todo_id},
+        json={
+            'todo_detail' : todo_detail
+        }
+    )
+    return response.json()
+
+def delete_todo(todo_id):
+    response = requests.delete(
+        f"{BASE_URL}/todos",
+        params={'todo_id':todo_id}
+    )

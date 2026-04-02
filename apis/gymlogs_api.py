8192b5485fbf65f8e4983d20b5db0913f07b5185
adityasingh2929 <2022aditya29@gmail.com>
2026-04-02 12:01:36 +0530

    added the deployed api endpoint instead of local endpoint to fix the bug

--- apis/gymlogs_api.py
+++ apis/gymlogs_api.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://productivity-manager-backend-ltmr.onrender.com"
 
 def create_week():
     response = requests.post(url=f"{BASE_URL}/gym/weeks")
